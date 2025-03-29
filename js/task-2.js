df991960b307afea265204f70e570438e01682e2
Ann K <kubareva.ann@gmail.com>
2025-03-29 21:08:36 +0200

    JS hw-4, fixed task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,7 @@
 function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
+  }
   const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
   return totalCalories / days.length;
 }
